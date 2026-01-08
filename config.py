25d9b583f2e75a538b77045923030704642df379
Dimaaap <procdima49@gmail.com>
2026-01-08 11:06:38 +0200

    removed print in config

--- config.py
+++ config.py
@@ -4,4 +4,3 @@ import redis.asyncio as redis
 
 HOST, PORT, PASSWORD = os.getenv("REDIS_HOST"), os.getenv("REDIS_PORT"), os.getenv("REDIS_PASSWORD")
 redis_client = redis.Redis(host=HOST, port=int(PORT), password=PASSWORD, db=0, decode_responses=True)
-print(redis_client)
